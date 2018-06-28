8d43ab27ada0b8bf80b77bba424d42ddf585bb66
Soheil Khodaei <soheilkd@outlook.com>
2018-06-28 23:10:42 +0430

    More modular architecture

--- MediaManager.cs
+++ MediaManager.cs
@@ -2,202 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows;
 
 namespace Player
 {
-	[Flags]
-	public enum MediaType
-	{
-		None = 0B0,
-		File = 0B10,
-		Music = 0B100 | File,
-		Video = 0B1000 | File,
-		OnlineFile = 0B10000 | File,
-		OnlineMusic = Music | OnlineFile,
-		OnlineVideo = Video | OnlineFile
-	}
-
-	[Serializable]
-	public class Media : INotifyPropertyChanged
-	{
-		protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-		{
-			field = value;
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-		public Media() { }
-		private string _Name;
-		public string Name { get => _Name; private set => Set(ref _Name, value); }
-		private string _Artist;
-		public string Artist { get => _Artist; private set => Set(ref _Artist, value); }
-		private string _Title;
-		public string Title { get => _Title; set => Set(ref _Title, value); }
-		private string _Album;
-		public string Album { get => _Album; private set => Set(ref _Album, value); }
-		private string _Directory;
-		public string Directory { get => _Directory; private set => Set(ref _Directory, value); }
-		private TimeSpan _Len;
-		public TimeSpan Length { get => _Len; set => Set(ref _Len, value); }
-		private int _PlayCount;
-		public int PlayCount { get => _PlayCount; set => Set(ref _PlayCount, value); }
-		[NonSerialized]
-		private bool _IsPlaying;
-		public bool IsPlaying { get => _IsPlaying; set => Set(ref _IsPlaying, value); }
-		public DateTime AdditionDate { get; set; }
-		private Uri _Url;
-		public Uri Url
-		{
-			get => _Url;
-			set
-			{
-				if (value.IsFile)
-					_Url = value;
-				else
-				{
-					if (value.AbsoluteUri[4] == 's')
-						_Url = new Uri(value.AbsoluteUri.Remove(4, 1));
-					else _Url = value;
-				}
-			}
-		}
-		public bool IsOffline => Url.IsFile;
-		public MediaType Type;
-		public string Path => Url.IsFile ? Url.LocalPath : Url.AbsoluteUri;
-		[NonSerialized] public string Lyrics = "";
-		[NonSerialized] public bool IsLoaded = false;
-		[NonSerialized] public System.Windows.Media.Imaging.BitmapSource Artwork;
-		public bool IsVideo => Type == MediaType.Video;
-		public bool Exists
-		{
-			get
-			{
-				if (Url.IsFile)
-					return File.Exists(Path);
-				else
-					return DownloadManager.IsDownloadable(Url, out _);
-			}
-		}
-		
-		[field: NonSerialized]
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		public Media(string path) : this(new Uri(path)) { }
-		public Media(Uri url)
-		{
-			Url = url;
-			AdditionDate = DateTime.Now;
-			Load();
-		}
-		public override string ToString() => $"{Artist} - {Title}";
-
-		public void MoveTo(string dir)
-		{
-			dir += Name;
-			File.Move(Path, dir);
-			Url = new Uri(dir);
-		}
-		public void CopyTo(string dir)
-		{
-			dir += Name;
-			File.Copy(Path, dir, true);
-		}
-		public void Load()
-		{
-			if (IsLoaded)
-				return;
-			if (IsOffline)
-			{
-				if (!Exists)
-				{
-					Name = null;
-					Directory = null;
-					Artist = null;
-					Title = null;
-					Album = null;
-					Artwork = null;
-					Type = MediaType.None;
-					Lyrics = null;
-				}
-
-				Name = Path.Substring(Path.LastIndexOf("\\") + 1);
-				Directory = Path.Substring(0, Path.LastIndexOf("\\"));
-				switch (MediaManager.GetMediaType(Url))
-				{
-					case MediaType.Music:
-						using (var t = TagLib.File.Create(Path))
-						{
-							Artist = t.Tag.FirstPerformer ?? Path.Substring(0, Path.LastIndexOf("\\"));
-							Title = t.Tag.Title ?? Name.Substring(0, Name.LastIndexOf("."));
-							Album = t.Tag.Album ?? String.Empty;
-							Artwork = t.Tag.Pictures.Length >= 1 ? Images.GetBitmap(t.Tag.Pictures[0]) : Images.MusicArt;
-							Type = MediaType.Music;
-							Lyrics = t.Tag.Lyrics ?? String.Empty;
-						}
-						break;
-					case MediaType.Video:
-						Title = Name;
-						Artist = Path.Substring(0, Path.LastIndexOf("\\"));
-						Artist = Artist.Substring(Artist.LastIndexOf("\\") + 1);
-						Album = "Video";
-						Artwork = Images.VideoArt;
-						Type = MediaType.Video;
-						Length = TimeSpan.Zero;
-						break;
-					default: break;
-				}
-				IsLoaded = true;
-			}
-			else
-			{
-				if (DownloadManager.IsDownloadable(Url, out Type))
-				{
-					Name = Uri.UnescapeDataString(Url.Segments.Last());
-					Title = Name;
-					Url = Url;
-					Artist = Url.Host;
-					Album = "Cloud";
-					Artwork = Images.NetArt;
-					IsLoaded = true;
-				}
-			}
-		}
-		public void Reload()
-		{
-			IsLoaded = false;
-			Load();
-		}
-
-		public Media Shallow => MemberwiseClone() as Media;
-	}
-
 	public enum PlayMode { Shuffle, RepeatOne, Repeat }
-	public enum QueueType { Unordered, Artist, Album, Type, Dir, Title }
+
 	public class MediaManager : ObservableCollection<Media>
 	{
 		public MediaManager()
 		{
 			LibraryOperator.Load().Unordered.For(each => Add(each));
-			DownloadManager.DownloadCompleted += DownloadManager_DownloadCompleted;
-		}
-
-		private void DownloadManager_DownloadCompleted(object sender, InfoExchangeArgs e)
-		{
-			switch (e.Type)
-			{
-				case InfoType.Media:
-					Add(e.Object as Media);
-					break;
-				case InfoType.MediaCollection:
-					e.Object.As<Media[]>().For(each => Add(each));
-					break;
-				default: break;
-			}
+			DownloadManager.DownloadCompleted += DownloadCompleted;
 		}
 
 		public Collection<Media> Queue;
@@ -211,7 +30,7 @@ namespace Player
 			if (Directory.Exists(path))
 				Directory.GetFiles(path, "*", SearchOption.AllDirectories).For(each => Add(each));
 			var media = new Media(path);
-			if (!media.Exists)
+			if (DoesExists(media))
 				return;
 			var duplication = this.Where(item => item.Path == path);
 			if (duplication.Count() != 0 && requestPlay)
@@ -290,14 +109,19 @@ namespace Player
 
 		public void UpdateOnPath(Media source)
 		{
-			this.For(each => each.Reload(), each => each.Path == source.Path);
+			this.For(each => Reload(each), each => each.Path == source.Path);
 		}
 		public void Revalidate()
 		{
-			this.For(each => each.Reload());
-			this.For(each => Remove(each), each => !each.Exists);
+			this.For(each => Reload(each));
+			this.For(each => Remove(each), each => !DoesExists(each));
 		}
-
+		
+		private void DownloadCompleted(object sender, InfoExchangeArgs e)
+		{
+			if (e.Type == InfoType.Media) Add(e.Object as Media);
+			else e.Object.As<Media[]>().For(each => Add(each));
+		}
 		#region Singular Media Operations
 		private static readonly string[] SupportedMusics = "mp3;wma;aac;m4a".Split(';');
 		private static readonly string[] SupportedVideos = "mp4;mpg;mkv;wmv;mov;avi;m4v;ts;wav;mpeg;webm".Split(';');
@@ -326,7 +150,6 @@ namespace Player
 			else
 				return url.Segments.Last().Substring(url.Segments.Last().LastIndexOf('.') + 1).ToLower();
 		}
-		public static bool IsMedia(string path) => new Media(path).Type != MediaType.None;
 
 		public static void CleanTag(Media media, bool prompt = true)
 		{
@@ -415,8 +238,91 @@ namespace Player
 						return;
 				}
 				file.Save();
-				media.Reload();
+				Reload(media);
 			}
+		}
+
+		public static void Load(Media media)
+		{
+			if (media.IsLoaded)
+				return;
+			if (media.IsOffline)
+			{
+				media.Name = media.Path.Substring(media.Path.LastIndexOf("\\") + 1);
+				media.Directory = media.Path.Substring(0, media.Path.LastIndexOf("\\"));
+				switch (MediaManager.GetMediaType(media.Url))
+				{
+					case MediaType.Music:
+						using (var t = TagLib.File.Create(media.Path))
+						{
+							media.Artist = t.Tag.FirstPerformer ?? media.Path.Substring(0, media.Path.LastIndexOf("\\"));
+							media.Title = t.Tag.Title ?? media.Name.Substring(0, media.Name.LastIndexOf("."));
+							media.Album = t.Tag.Album ?? String.Empty;
+							media.Artwork = t.Tag.Pictures.Length >= 1 ? Images.GetBitmap(t.Tag.Pictures[0]) : Images.MusicArt;
+							media.Type = MediaType.Music;
+							media.Lyrics = t.Tag.Lyrics ?? String.Empty;
+						}
+						break;
+					case MediaType.Video:
+						media.Title = media.Name;
+						media.Artist = media.Path.Substring(0, media.Path.LastIndexOf("\\"));
+						media.Artist = media.Artist.Substring(media.Artist.LastIndexOf("\\") + 1);
+						media.Album = "Video";
+						media.Artwork = Images.VideoArt;
+						media.Type = MediaType.Video;
+						media.Length = TimeSpan.Zero;
+						break;
+					default: break;
+				}
+				media.IsLoaded = true;
+			}
+			else
+			{
+				if (DownloadManager.IsDownloadable(media.Url, out media.Type))
+				{
+					media.Name = Uri.UnescapeDataString(media.Url.Segments.Last());
+					media.Title = media.Name;
+					media.Artist = media.Url.Host;
+					media.Album = "Cloud";
+					media.Artwork = Images.NetArt;
+					media.IsLoaded = true;
+				}
+			}
+		}
+		public static void Reload(Media media)
+		{
+			media.IsLoaded = false;
+			Load(media);
+		}
+		public static void Move(Media media, string toDir)
+		{
+			toDir += media.Name;
+			File.Move(media.Path, toDir);
+			media.Url = new Uri(toDir);
+		}
+		public static void Copy(Media media, string toDir)
+		{
+			toDir += media.Name;
+			File.Copy(media.Path, toDir, true);
+		}
+		public static bool DoesExists(Uri url)
+		{
+			if (url.IsFile)
+				return File.Exists(url.AbsolutePath);
+			else
+				return DownloadManager.IsDownloadable(url, out _);
+		}
+		public static bool DoesExists(Media media) => DoesExists(media.Url);
+
+		public static Media CreateMedia(string path)
+		{
+			var m = new Media(path);
+			Load(m);
+			return m;
+		}
+		public static bool IsMedia(string path)
+		{
+			return GetMediaType(new Uri(path)) == MediaType.File;
 		}
 		#endregion
 	}
---
+++ Models/Media.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+
+namespace Player
+{
+	[Flags]
+	public enum MediaType
+	{
+		None = 0B0,
+		File = 0B10,
+		Music = 0B100 | File,
+		Video = 0B1000 | File,
+		OnlineFile = 0B10000 | File,
+		OnlineMusic = Music | OnlineFile,
+		OnlineVideo = Video | OnlineFile
+	}
+
+	[Serializable]
+	public class Media : INotifyPropertyChanged
+	{
+		protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private string _Name;
+		private string _Artist;
+		private string _Title;
+		private string _Album;
+		private string _Dir;
+		private int _PlayCount;
+		private bool _IsPlaying;
+		private TimeSpan _Len;
+		public string Name { get => _Name; set => Set(ref _Name, value); }
+		public string Artist { get => _Artist; set => Set(ref _Artist, value); }
+		public string Title { get => _Title; set => Set(ref _Title, value); }
+		public string Album { get => _Album; set => Set(ref _Album, value); }
+		public string Directory { get => _Dir; set => Set(ref _Dir, value); }
+		public int PlayCount { get => _PlayCount; set => Set(ref _PlayCount, value); }
+		public bool IsPlaying { get => _IsPlaying; set => Set(ref _IsPlaying, value); }
+		public TimeSpan Length { get => _Len; set => Set(ref _Len, value); }
+		public DateTime AdditionDate { get; private set; }
+		public Uri Url { get; set; }
+		public bool IsOffline => Url.IsFile;
+		public MediaType Type;
+		public string Path => Url.IsFile ? Url.LocalPath : Url.AbsoluteUri;
+		public bool IsVideo => Type == MediaType.Video;
+		[field: NonSerialized] public string Lyrics = "";
+		[field: NonSerialized] public bool IsLoaded = false;
+		[field: NonSerialized] public BitmapSource Artwork;
+		[field: NonSerialized] public event PropertyChangedEventHandler PropertyChanged;
+
+		public Media() { }
+		public Media(Uri url)
+		{
+			//Just because defualt .Net webClient doesn't support https protocol
+			if (!url.IsFile && url.AbsoluteUri[4] == 's')
+				Url = new Uri(url.AbsoluteUri.Remove(4, 1));
+			else Url = url;
+			Url = url;
+			AdditionDate = DateTime.Now;
+		}
+		public Media(string path) : this(new Uri(path)) { }
+
+		public override string ToString() => $"{Artist} - {Title}";
+	}
+}
