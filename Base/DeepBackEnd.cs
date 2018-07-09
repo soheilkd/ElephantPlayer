acd7334b1471484b3453ad5205a7b8bce8a9ac3d
Soheil Khodaei <soheilkd@outlook.com>
2018-07-09 12:05:16 +0430

    Seperated taskbar code-base

--- Base/DeepBackEnd.cs
+++ Base/DeepBackEnd.cs
@@ -1,5 +1,4 @@
-﻿using MaterialDesignThemes.Wpf;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,31 +12,108 @@ using System.Runtime.Serialization.Formatters;
 using System.Security;
 using System.Threading;
 using System.Windows;
-using System.Windows.Input;
-using System.Windows.Shell;
 using System.Windows.Threading;
 
 namespace Player.DeepBackEnd
 {
 	internal enum WM
 	{
-		NULL = 0x0000, CREATE = 0x0001, DESTROY = 0x0002, MOVE = 0x0003, SIZE = 0x0005, ACTIVATE = 0x0006,
-		SETFOCUS = 0x0007, KILLFOCUS = 0x0008, ENABLE = 0x000A, SETREDRAW = 0x000B, SETTEXT = 0x000C, GETTEXT = 0x000D,
-		GETTEXTLENGTH = 0x000E, PAINT = 0x000F, CLOSE = 0x0010, QUERYENDSESSION = 0x0011, QUIT = 0x0012, QUERYOPEN = 0x0013,
-		ERASEBKGND = 0x0014, SYSCOLORCHANGE = 0x0015, SHOWWINDOW = 0x0018, ACTIVATEAPP = 0x001C, SETCURSOR = 0x0020, MOUSEACTIVATE = 0x0021,
-		CHILDACTIVATE = 0x0022, QUEUESYNC = 0x0023, GETMINMAXINFO = 0x0024, WINDOWPOSCHANGING = 0x0046, WINDOWPOSCHANGED = 0x0047,
-		CONTEXTMENU = 0x007B, STYLECHANGING = 0x007C, STYLECHANGED = 0x007D, DISPLAYCHANGE = 0x007E, GETICON = 0x007F, SETICON = 0x0080,
-		NCCREATE = 0x0081, NCDESTROY = 0x0082, NCCALCSIZE = 0x0083, NCHITTEST = 0x0084, NCPAINT = 0x0085, NCACTIVATE = 0x0086,
-		GETDLGCODE = 0x0087, SYNCPAINT = 0x0088, NCMOUSEMOVE = 0x00A0, NCLBUTTONDOWN = 0x00A1, NCLBUTTONUP = 0x00A2, NCLBUTTONDBLCLK = 0x00A3,
-		NCRBUTTONDOWN = 0x00A4, NCRBUTTONUP = 0x00A5, NCRBUTTONDBLCLK = 0x00A6, NCMBUTTONDOWN = 0x00A7, NCMBUTTONUP = 0x00A8, NCMBUTTONDBLCLK = 0x00A9,
-		SYSKEYDOWN = 0x0104, SYSKEYUP = 0x0105, SYSCHAR = 0x0106, SYSDEADCHAR = 0x0107, COMMAND = 0x0111, SYSCOMMAND = 0x0112,
-		LBUTTONDOWN = 0x0201, LBUTTONUP = 0x0202, LBUTTONDBLCLK = 0x0203, RBUTTONDOWN = 0x0204, RBUTTONUP = 0x0205, RBUTTONDBLCLK = 0x0206,
-		MBUTTONDOWN = 0x0207, MBUTTONUP = 0x0208, MBUTTONDBLCLK = 0x0209, MOUSEWHEEL = 0x020A, MOUSEHWHEEL = 0x020E, MOUSEMOVE = 0x0200,
-		XBUTTONDOWN = 0x020B, XBUTTONUP = 0x020C, XBUTTONDBLCLK = 0x020D, CAPTURECHANGED = 0x0215, ENTERSIZEMOVE = 0x0231, EXITSIZEMOVE = 0x0232,
-		IME_SETCONTEXT = 0x0281, IME_NOTIFY = 0x0282, IME_CONTROL = 0x0283, IME_COMPOSITIONFULL = 0x0284, IME_SELECT = 0x0285, IME_CHAR = 0x0286,
-		IME_REQUEST = 0x0288, IME_KEYDOWN = 0x0290, IME_KEYUP = 0x0291, NCMOUSELEAVE = 0x02A2, USER = 0x0400, TRAYMOUSEMESSAGE = 0x800, APP = 0x8000,
-		DWMCOMPOSITIONCHANGED = 0x031E, DWMNCRENDERINGCHANGED = 0x031F, DWMCOLORIZATIONCOLORCHANGED = 0x0320,
-		DWMWINDOWMAXIMIZEDCHANGE = 0x0321, DWMSENDICONICTHUMBNAIL = 0x0323, DWMSENDICONICLIVEPREVIEWBITMAP = 0x0326
+		NULL = 0x0000,
+		CREATE = 0x0001,
+		DESTROY = 0x0002,
+		MOVE = 0x0003,
+		SIZE = 0x0005,
+		ACTIVATE = 0x0006,
+		SETFOCUS = 0x0007,
+		KILLFOCUS = 0x0008,
+		ENABLE = 0x000A,
+		SETREDRAW = 0x000B,
+		SETTEXT = 0x000C,
+		GETTEXT = 0x000D,
+		GETTEXTLENGTH = 0x000E,
+		PAINT = 0x000F,
+		CLOSE = 0x0010,
+		QUERYENDSESSION = 0x0011,
+		QUIT = 0x0012,
+		QUERYOPEN = 0x0013,
+		ERASEBKGND = 0x0014,
+		SYSCOLORCHANGE = 0x0015,
+		SHOWWINDOW = 0x0018,
+		ACTIVATEAPP = 0x001C,
+		SETCURSOR = 0x0020,
+		MOUSEACTIVATE = 0x0021,
+		CHILDACTIVATE = 0x0022,
+		QUEUESYNC = 0x0023,
+		GETMINMAXINFO = 0x0024,
+		WINDOWPOSCHANGING = 0x0046,
+		WINDOWPOSCHANGED = 0x0047,
+		CONTEXTMENU = 0x007B,
+		STYLECHANGING = 0x007C,
+		STYLECHANGED = 0x007D,
+		DISPLAYCHANGE = 0x007E,
+		GETICON = 0x007F,
+		SETICON = 0x0080,
+		NCCREATE = 0x0081,
+		NCDESTROY = 0x0082,
+		NCCALCSIZE = 0x0083,
+		NCHITTEST = 0x0084,
+		NCPAINT = 0x0085,
+		NCACTIVATE = 0x0086,
+		GETDLGCODE = 0x0087,
+		SYNCPAINT = 0x0088,
+		NCMOUSEMOVE = 0x00A0,
+		NCLBUTTONDOWN = 0x00A1,
+		NCLBUTTONUP = 0x00A2,
+		NCLBUTTONDBLCLK = 0x00A3,
+		NCRBUTTONDOWN = 0x00A4,
+		NCRBUTTONUP = 0x00A5,
+		NCRBUTTONDBLCLK = 0x00A6,
+		NCMBUTTONDOWN = 0x00A7,
+		NCMBUTTONUP = 0x00A8,
+		NCMBUTTONDBLCLK = 0x00A9,
+		SYSKEYDOWN = 0x0104,
+		SYSKEYUP = 0x0105,
+		SYSCHAR = 0x0106,
+		SYSDEADCHAR = 0x0107,
+		COMMAND = 0x0111,
+		SYSCOMMAND = 0x0112,
+		LBUTTONDOWN = 0x0201,
+		LBUTTONUP = 0x0202,
+		LBUTTONDBLCLK = 0x0203,
+		RBUTTONDOWN = 0x0204,
+		RBUTTONUP = 0x0205,
+		RBUTTONDBLCLK = 0x0206,
+		MBUTTONDOWN = 0x0207,
+		MBUTTONUP = 0x0208,
+		MBUTTONDBLCLK = 0x0209,
+		MOUSEWHEEL = 0x020A,
+		MOUSEHWHEEL = 0x020E,
+		MOUSEMOVE = 0x0200,
+		XBUTTONDOWN = 0x020B,
+		XBUTTONUP = 0x020C,
+		XBUTTONDBLCLK = 0x020D,
+		CAPTURECHANGED = 0x0215,
+		ENTERSIZEMOVE = 0x0231,
+		EXITSIZEMOVE = 0x0232,
+		IME_SETCONTEXT = 0x0281,
+		IME_NOTIFY = 0x0282,
+		IME_CONTROL = 0x0283,
+		IME_COMPOSITIONFULL = 0x0284,
+		IME_SELECT = 0x0285,
+		IME_CHAR = 0x0286,
+		IME_REQUEST = 0x0288,
+		IME_KEYDOWN = 0x0290,
+		IME_KEYUP = 0x0291,
+		NCMOUSELEAVE = 0x02A2,
+		USER = 0x0400,
+		TRAYMOUSEMESSAGE = 0x800,
+		APP = 0x8000,
+		DWMCOMPOSITIONCHANGED = 0x031E,
+		DWMNCRENDERINGCHANGED = 0x031F,
+		DWMCOLORIZATIONCOLORCHANGED = 0x0320,
+		DWMWINDOWMAXIMIZEDCHANGE = 0x0321,
+		DWMSENDICONICTHUMBNAIL = 0x0323,
+		DWMSENDICONICLIVEPREVIEWBITMAP = 0x0326
 	}
 	[SuppressUnmanagedCodeSecurity]
 	internal static class NativeMethods
@@ -76,7 +152,6 @@ namespace Player.DeepBackEnd
 			}
 		}
 	}
-
 }
 
 namespace Player.DeepBackEnd.InstanceManagement
@@ -204,73 +279,5 @@ namespace Player.DeepBackEnd.InstanceManagement
 			}
 			public override object InitializeLifetimeService() => null;
 		}
-	}
-}
-
-namespace Player.Taskbar
-{
-	public class Command : ICommand
-	{
-#pragma warning disable CS0067 //Suppres never used warning
-		public event EventHandler CanExecuteChanged;
-#pragma warning restore CS0067
-		public event EventHandler Raised;
-		public bool CanExecute(object parameter) => true;
-		public void Execute(object parameter) => Raised?.Invoke(this, null);
-	}
-	public class Thumb
-	{
-		public event EventHandler PlayPressed;
-		public event EventHandler PausePressed;
-		public event EventHandler PrevPressed;
-		public event EventHandler NextPressed;
-
-		public ThumbButtonInfo PlayThumb = new ThumbButtonInfo()
-		{
-			Description = "Play",
-			ImageSource = Images.GetBitmap(PackIconKind.Play)
-		};
-		public ThumbButtonInfo PauseThumb = new ThumbButtonInfo()
-		{
-			Description = "Pause",
-			ImageSource = Images.GetBitmap(PackIconKind.Pause)
-		};
-		public ThumbButtonInfo PreviousThumb = new ThumbButtonInfo()
-		{
-			Description = "Previous",
-			ImageSource = Images.GetBitmap(PackIconKind.SkipPrevious)
-		};
-		public ThumbButtonInfo NextThumb = new ThumbButtonInfo()
-		{
-			Description = "Next",
-			ImageSource = Images.GetBitmap(PackIconKind.SkipNext)
-		};
-
-		private Command PlayHandler = new Command();
-		private Command PauseHandler = new Command();
-		private Command PrevHandler = new Command();
-		private Command NextHandler = new Command();
-		public TaskbarItemInfo Info { get; } = new TaskbarItemInfo();
-
-		public Thumb()
-		{
-			PlayHandler.Raised += (sender, e) => PlayPressed?.Invoke(sender, e);
-			PauseHandler.Raised += (sender, e) => PausePressed?.Invoke(sender, e);
-			PrevHandler.Raised += (sender, e) => PrevPressed?.Invoke(sender, e);
-			NextHandler.Raised += (sender, e) => NextPressed?.Invoke(sender, e);
-			PreviousThumb.Command = PrevHandler;
-			NextThumb.Command = NextHandler;
-			PlayThumb.Command = PlayHandler;
-			PauseThumb.Command = PauseHandler;
-
-			Info.ThumbButtonInfos.Clear();
-			Info.ThumbButtonInfos.Add(PreviousThumb);
-			Info.ThumbButtonInfos.Add(PlayThumb);
-			Info.ThumbButtonInfos.Add(NextThumb);
-		}
-
-		public void SetPlayingState(bool IsPlaying = false) => Info.ThumbButtonInfos[1] = IsPlaying ? PauseThumb : PlayThumb;
-		public void SetProgressState(TaskbarItemProgressState state) => Info.ProgressState = state;
-		public void SetProgressValue(double value) => Info.ProgressValue = value;
 	}
 }
---
+++ Base/Taskbar.cs
@@ -0,0 +1,72 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Windows.Input;
+using System.Windows.Shell;
+
+namespace Player.Taskbar
+{
+	public class Command : ICommand
+	{
+#pragma warning disable CS0067 //Suppres never used warning
+		public event EventHandler CanExecuteChanged;
+#pragma warning restore CS0067
+		public event EventHandler Raised;
+		public bool CanExecute(object parameter) => true;
+		public void Execute(object parameter) => Raised?.Invoke(this, null);
+	}
+	public class Thumb
+	{
+		public event EventHandler PlayPressed;
+		public event EventHandler PausePressed;
+		public event EventHandler PrevPressed;
+		public event EventHandler NextPressed;
+
+		public ThumbButtonInfo PlayThumb = new ThumbButtonInfo()
+		{
+			Description = "Play",
+			ImageSource = Images.GetBitmap(PackIconKind.Play)
+		};
+		public ThumbButtonInfo PauseThumb = new ThumbButtonInfo()
+		{
+			Description = "Pause",
+			ImageSource = Images.GetBitmap(PackIconKind.Pause)
+		};
+		public ThumbButtonInfo PreviousThumb = new ThumbButtonInfo()
+		{
+			Description = "Previous",
+			ImageSource = Images.GetBitmap(PackIconKind.SkipPrevious)
+		};
+		public ThumbButtonInfo NextThumb = new ThumbButtonInfo()
+		{
+			Description = "Next",
+			ImageSource = Images.GetBitmap(PackIconKind.SkipNext)
+		};
+
+		private Command PlayHandler = new Command();
+		private Command PauseHandler = new Command();
+		private Command PrevHandler = new Command();
+		private Command NextHandler = new Command();
+		public TaskbarItemInfo Info { get; } = new TaskbarItemInfo();
+
+		public Thumb()
+		{
+			PlayHandler.Raised += (sender, e) => PlayPressed?.Invoke(sender, e);
+			PauseHandler.Raised += (sender, e) => PausePressed?.Invoke(sender, e);
+			PrevHandler.Raised += (sender, e) => PrevPressed?.Invoke(sender, e);
+			NextHandler.Raised += (sender, e) => NextPressed?.Invoke(sender, e);
+			PreviousThumb.Command = PrevHandler;
+			NextThumb.Command = NextHandler;
+			PlayThumb.Command = PlayHandler;
+			PauseThumb.Command = PauseHandler;
+
+			Info.ThumbButtonInfos.Clear();
+			Info.ThumbButtonInfos.Add(PreviousThumb);
+			Info.ThumbButtonInfos.Add(PlayThumb);
+			Info.ThumbButtonInfos.Add(NextThumb);
+		}
+
+		public void SetPlayingState(bool IsPlaying = false) => Info.ThumbButtonInfos[1] = IsPlaying ? PauseThumb : PlayThumb;
+		public void SetProgressState(TaskbarItemProgressState state) => Info.ProgressState = state;
+		public void SetProgressValue(double value) => Info.ProgressValue = value;
+	}
+}
