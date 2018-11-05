7d72738b47cbd9ffcf171e5024a8a1ac048f88d5
Soheil Khodaei <soheilkd@outlook.com>
2018-11-05 22:39:59 +0330

    Added ThumbController

---
+++ Library/ThumbController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Shell;
+using Library.Controls;
+
+namespace Player
+{
+	public class ThumbController
+	{
+		private TaskbarItemInfo _Info;
+
+		public event EventHandler PlayPauseClicked;
+		public event EventHandler PreviousClicked;
+		public event EventHandler NextClicked;
+
+		private readonly ThumbButtonInfo _PlayThumb = new ThumbButtonInfo()
+		{
+			Description = "Play",
+			ImageSource = IconProvider.GetBitmap(IconType.Play)
+		};
+		private readonly ThumbButtonInfo _PauseThumb = new ThumbButtonInfo()
+		{
+			Description = "Pause",
+			ImageSource = IconProvider.GetBitmap(IconType.Pause)
+		};
+		private readonly ThumbButtonInfo _PreviousThumb = new ThumbButtonInfo()
+		{
+			Description = "Previous",
+			ImageSource = IconProvider.GetBitmap(IconType.Previous)
+		};
+		private readonly ThumbButtonInfo _NextThumb = new ThumbButtonInfo()
+		{
+			Description = "Next",
+			ImageSource = IconProvider.GetBitmap(IconType.Next)
+		};
+
+		public ThumbController(TaskbarItemInfo info)
+		{
+			info.ThumbButtonInfos.Add(_PreviousThumb);
+			info.ThumbButtonInfos.Add(_PlayThumb);
+			info.ThumbButtonInfos.Add(_NextThumb);
+			_PlayThumb.Click += (s, e) => PlayPauseClicked?.Invoke(s, e);
+			_PauseThumb.Click += (s, e) => PlayPauseClicked?.Invoke(s, e);
+			_PreviousThumb.Click += (s, e) => PreviousClicked?.Invoke(s, e);
+			_NextThumb.Click += (s, e) => NextClicked?.Invoke(s, e);
+			_Info = info;
+		}
+
+		public void SetPlayingStateOnThumb(bool isPlaying)
+		{
+			_Info.ThumbButtonInfos[1] = isPlaying ? _PauseThumb : _PlayThumb;
+		}
+	}
+}
